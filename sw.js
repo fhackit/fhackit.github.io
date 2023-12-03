6e3dad815d7ea4d59471fdd2de3bd8127f1330f7
Daniele D'Agnelli <daniele@bidimensional.net>
2023-12-03 18:45:31 +0000

    go

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'fhackit-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './fhackit_template.js',
+  './fhackit_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
